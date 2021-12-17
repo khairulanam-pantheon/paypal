1e8436dadf52a5385da003ddc1072b1cbcaffcc7
Khairul Anam <khairul.anam@pantheon.io>
2021-12-18 00:50:02 +0600

    update info

--- src/View/payment.js
+++ src/View/payment.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom";
 import {useParams, Route} from "react-router-dom";
 
 const PayPalButton = window.paypal.Buttons.driver("react", { React, ReactDOM });
 function Payment() {
   const {amt} = useParams();
+  const [orderInfo, setOrederInfo] = useState({})
+
+  useEffect(() => {
+    fetch('https://nzapi.romanaexpress.com/order-details/'+ amt +'?apiKey=Nokia%40123')
+    .then((response) => response.json())
+    .then((json) => {
+      setOrederInfo(json.data)
+      })
+  }, [])
+
+  if(orderInfo.billingAddress){
+    var billingPhone = orderInfo.billingAddress.phone.replace(/ /g, '')
+  }
+  
   function _createOrder(data, actions) {
     return actions.order.create({
       purchase_units: [
         {
           amount: {
-            value: amt,
+            value: orderInfo.order_total,
           },
         },
       ],
+      payer: {
+        name: {
+          given_name: orderInfo.billingAddress.first_name,
+          surname: orderInfo.billingAddress.last_name
+        },
+        address: {
+          address_line_1: orderInfo.billingAddress.address_1,
+          address_line_2: orderInfo.billingAddress.address_2,
+          admin_area_2: orderInfo.billingAddress.address_1,
+          admin_area_1: orderInfo.billingAddress.address_2,
+          postal_code: orderInfo.billingAddress.postcode,
+          country_code: orderInfo.billingAddress.countryName
+        },
+        email_address: orderInfo.billingAddress.email,
+        phone: {
+          phone_type: "MOBILE",
+          phone_number: {
+            national_number: billingPhone
+          }
+        }
+      }, 
     });
   }
   async function _onApprove(data, actions) {
